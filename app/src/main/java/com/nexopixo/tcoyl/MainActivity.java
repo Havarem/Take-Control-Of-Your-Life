cf0ddedd710801d9e5493d6ad5dee51c360571e2
Havarem <andre.jacques@nexopixo.com>
2019-04-05 15:54:37 -0400

    Adding all the basic elements
    This is a pure, basic "Empty Activity" project from Android Studio with
    all the file except the ones in the build/ folder. Still an experiment
    of the IDE capabitilies.

---
+++ app/src/main/java/com/nexopixo/tcoyl/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nexopixo.tcoyl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
